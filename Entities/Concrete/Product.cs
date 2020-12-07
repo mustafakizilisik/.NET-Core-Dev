55891c67377d00d8104b769197f8ca8b25edb8c0
Mustafa Kızılışık <mkizilisik@outlook.com>
2020-12-07 12:00:00 +0300

    deneme

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -13,5 +13,6 @@ namespace Entities.Concrete
         public string QuantityPerUnit { get; set; }
         public decimal UnitPrice { get; set; }
         public short UnitInStock { get; set; }
+
     }
 }
