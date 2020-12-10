3e6822632e50699c1ae3b028cc4318913d123ee1
Mustafa Kızılışık <mkizilisik@outlook.com>
2020-12-10 16:57:14 +0300

    Core katmanına İşlem başarı ve hata sistemin oluşturulması adına interface ve class'lar gerekli metodlar ile birlikte eklendi.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -18,35 +19,38 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public void Add(Product product)
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId)); 
+        }
+
+        public IDataResult<List<Product>> GetList()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList());
+        }
+
+        public IResult Add(Product product)
         {
             //Business codes
             _productDal.Add(product);
+            return new SuccessResult("Ürün başarıyla eklendi.");
         }
 
-        public void Delete(Product product)
+        public IResult Delete(Product product)
         {
             _productDal.Delete(product);
+            return new SuccessResult("Ürün başarıyla silindi.");
         }
 
-        public Product GetById(int productId)
-        {
-            return _productDal.Get(p => p.ProductId == productId);
-        }
-
-        public List<Product> GetList()
-        {
-            return _productDal.GetList().ToList();
-        }
-
-        public List<Product> GetListByCategory(int categoryId)
-        {
-            return _productDal.GetList(p => p.CategoryId == categoryId).ToList();
-        }
-
-        public void Update(Product product)
+        public IResult Update(Product product)
         {
             _productDal.Update(product);
+            return new SuccessResult("Ürün başarıyla güncellendi.");
         }
     }
 }
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //this(success) bu kod ile tek parametreli successi set eder
+        //Scope içerisindeki şekilde de kullanılabilir, kod tekrarı olmaz. 
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+            //Success = success
+        }
+        public Result (bool success)
+        {
+            Success = success;
+        }
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
