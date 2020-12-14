c0c5db54fcb5846ba869afc0cd73fe88d1ff9ae9
Mustafa Kızılışık <mkizilisik@outlook.com>
2020-12-15 00:37:32 +0300

    -Category tablosu metodları ile beraber projeye eklendi.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -17,6 +17,8 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<ProductManager>().As<IProductService>();
             builder.RegisterType<EfProductDal>().As<IProductDal>();
 
+            builder.RegisterType<CategoryManager>().As<ICategoryService>();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
         }
     }
 }
