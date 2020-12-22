37bcae059ac6f3e136e8ccd4f173ebdc7dd2b7aa
Mustafa Kızılışık <mkizilisik@outlook.com>
2020-12-22 15:43:06 +0300

    -JWT altyapısı eklendi. -Token Operasyonları tamamandı. -Login ve Register operasyonları tamamlandı. -Startup konfigürasyonu.

--- Core/Utilities/Results/SuccessDataResult.cs
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Utilities.Results
 {
-    public class SuccessDataResult<T>:DataResult<T>
+    public class SuccessDataResult<T> : DataResult<T>
     {
-        public SuccessDataResult(T data, bool success, string message) : base(data, true, message)
+        public SuccessDataResult(T data, string message) : base(data, true, message)
         {
 
         }
@@ -16,12 +17,12 @@ namespace Core.Utilities.Results
 
         }
 
-        public SuccessDataResult(string message):base(default, true, message)
+        public SuccessDataResult(string message) : base(default, true, message)
         {
 
         }
 
-        public SuccessDataResult():base(default, true)
+        public SuccessDataResult() : base(default, true)
         {
 
         }
